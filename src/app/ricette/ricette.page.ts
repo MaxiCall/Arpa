73238dfea0a2d5dece7501d5a75a7cd4f7e7aabd
MaxiCall <max.call87@gmail.com>
2025-07-05 11:49:16 +0200

    recipes page

--- src/app/ricette/ricette.page.ts
+++ src/app/ricette/ricette.page.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardTitle} from '@ionic/angular/standalone';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-ricette',
   templateUrl: './ricette.page.html',
   styleUrls: ['./ricette.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonCard, IonCardContent, IonCardTitle]
 })
 export class RicettePage implements OnInit {
 
-  constructor() { }
+    navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
